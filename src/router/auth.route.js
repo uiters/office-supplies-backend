3ab876d35678c5fd7656abcb8b32c49b5e017ea9
Phat Ngo <ntp18799@gmail.com>
2020-11-15 15:30:59 +0700

    nothing

--- src/router/auth.route.js
+++ src/router/auth.route.js
@@ -39,13 +39,13 @@ const router = require('express').Router();
 
 router.post('/login', async (req, res) => {
     let user = await User.findOne({email: req.body.email});
-    if(!user) return res.status(404).send('Invalid email!')
+    if(!user) return res.status(404).send({res:'Invalid email!'})
     const isCorrectPass = await user.comparePass(req.body.password, user.password);
-    if(!isCorrectPass) return res.status(404).send('Invalid password!')
+    if(!isCorrectPass) return res.status(404).send({res:'Invalid email!'})
 
     const token = user.createToken();
 
-    res.send(`bearer ${token}`);
+    res.send(token);
 })
 
 module.exports = router
