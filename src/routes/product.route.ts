a392d890bc825cf117935db4357e19faadc91b80
Phan Hoang <17520519@gm.uit.edu.vn>
2020-11-26 09:10:52 +0700

    remove authJwt from get products

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,21 +1,26 @@
-import express from 'express';
-import { authJwt } from '../config/passport';
-import ProductController from '../controller/product.controller';
-import { createProductValidate } from '../util/validatiors/product.validate';
+import express from "express";
+import { authJwt } from "../config/passport";
+import ProductController from "../controller/product.controller";
+import { createProductValidate } from "../util/validatiors/product.validate";
 
 const router = express.Router();
 const productController = new ProductController();
 
 // ** http://localhost:3000/api/product/?page=?&keyword=?&sortBy=?
-router.get('/', authJwt, productController.getProducts);
-router.get('/product-id/:id', authJwt, productController.getProductById);
+router.get("/", productController.getProducts);
+router.get("/product-id/:id", authJwt, productController.getProductById);
 
 // ** http://localhost:3000/api/product/user-products/?page=?&keyword=?&sortBy=?
-router.get('/user-products', authJwt, productController.getUserProducts)
+router.get("/user-products", authJwt, productController.getUserProducts);
 
-router.post('/', authJwt, createProductValidate,productController.createProduct);
-router.put('/', authJwt, productController.updateProduct);
+router.post(
+  "/",
+  authJwt,
+  createProductValidate,
+  productController.createProduct
+);
+router.put("/", authJwt, productController.updateProduct);
 
-router.delete('/:id', authJwt, productController.deleteProduct);
+router.delete("/:id", authJwt, productController.deleteProduct);
 
 export default router;
