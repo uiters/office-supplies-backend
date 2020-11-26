b4a646fd52538e78bd8bbad0a29ea96b5fb778c0
Phan Hoang <17520519@gm.uit.edu.vn>
2020-11-26 10:06:29 +0700

    small fix

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,26 +1,29 @@
-import express from 'express';
-import { authJwt } from '../config/passport';
-import { UserController } from '../controller/user.controller';
-import { isAdmin } from '../util/permission.util';
-import { createUserValidators, editUserValidators } from '../util/validatiors/user.validate';
+import express from "express";
+import { authJwt } from "../config/passport";
+import { UserController } from "../controller/user.controller";
+import { isAdmin } from "../util/permission.util";
+import {
+  createUserValidators,
+  editUserValidators
+} from "../util/validatiors/user.validate";
 
 const router = express.Router();
 
 const userController = new UserController();
 
-router.get('/user-id/:id', authJwt, userController.getUserById);
-router.get('/me', authJwt, userController.getMe);
-router.get('/email-verification/:token', userController.verifyUser);
-router.get('/reset-password/:token', userController.resetPassword);
+router.get("/user-id/:id", authJwt, userController.getUserById);
+router.get("/me", authJwt, userController.getMe);
+router.get("/email-verification/:token", userController.verifyUser);
+router.get("/reset-password/:token", userController.resetPassword);
 
 // http://localhost:3000/user/?page=
-router.get('/', authJwt, isAdmin, userController.getUser);
+router.get("/", authJwt, isAdmin, userController.getUser);
 
-router.put('/', authJwt, editUserValidators, userController.updateUser);
-router.put('/change-password', authJwt, userController.changePassword);
-router.post('/', createUserValidators, userController.createUser);
-router.post('/forgot-password', userController.forgotPassword);
+router.put("/", authJwt, userController.updateUser);
+router.put("/change-password", authJwt, userController.changePassword);
+router.post("/", createUserValidators, userController.createUser);
+router.post("/forgot-password", userController.forgotPassword);
 
-router.delete('/:id', authJwt, isAdmin, userController.deleteUser);
+router.delete("/:id", authJwt, isAdmin, userController.deleteUser);
 
 export default router;
