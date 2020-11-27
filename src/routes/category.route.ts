37965510df67a706ec49f5135ea0bac16afdb9ac
Phan Hoang <17520519@gm.uit.edu.vn>
2020-11-27 09:10:50 +0700

    remove authjwt from product-type and category

--- src/routes/category.route.ts
+++ src/routes/category.route.ts
@@ -1,22 +1,34 @@
-import express from 'express';
-import { authJwt } from '../config/passport';
-import CategoryController from '../controller/category.controller';
-import { isAdmin } from '../util/permission.util';
+import express from "express";
+import { authJwt } from "../config/passport";
+import CategoryController from "../controller/category.controller";
+import { isAdmin } from "../util/permission.util";
 import {
-    createCategoryValidate,
-    updateCategoryValidate,
-} from '../util/validatiors/category.validate';
+  createCategoryValidate,
+  updateCategoryValidate
+} from "../util/validatiors/category.validate";
 
 const router = express.Router();
 
 const categoryController = new CategoryController();
 
-router.get('/', authJwt, categoryController.getCategories);
-router.get('/:id', authJwt, categoryController.getCategoryById);
+router.get("/", categoryController.getCategories);
+router.get("/:id", authJwt, categoryController.getCategoryById);
 
-router.put('/', authJwt, isAdmin, updateCategoryValidate, categoryController.updateCategory);
-router.post('/', authJwt, isAdmin, createCategoryValidate, categoryController.createCategory);
+router.put(
+  "/",
+  authJwt,
+  isAdmin,
+  updateCategoryValidate,
+  categoryController.updateCategory
+);
+router.post(
+  "/",
+  authJwt,
+  isAdmin,
+  createCategoryValidate,
+  categoryController.createCategory
+);
 
-router.patch('/', authJwt, categoryController.getCategoriesByType);
-router.delete('/:id', authJwt, isAdmin, categoryController.deleteCategory);
+router.patch("/", authJwt, categoryController.getCategoriesByType);
+router.delete("/:id", authJwt, isAdmin, categoryController.deleteCategory);
 export default router;
--- src/routes/productType.route.ts
+++ src/routes/productType.route.ts
@@ -1,17 +1,17 @@
-import express from 'express';
-import { authJwt } from '../config/passport';
-import ProductTypeController from '../controller/productType.controller';
-import { isAdmin } from '../util/permission.util';
-import { typeValidate } from '../util/validatiors/productType.validate';
+import express from "express";
+import { authJwt } from "../config/passport";
+import ProductTypeController from "../controller/productType.controller";
+import { isAdmin } from "../util/permission.util";
+import { typeValidate } from "../util/validatiors/productType.validate";
 
 const router = express.Router();
 const typeController = new ProductTypeController();
 
-router.get('/', authJwt, typeController.getType);
+router.get("/", typeController.getType);
 
-router.post('/', authJwt, isAdmin, typeValidate, typeController.createType);
-router.put('/', authJwt, isAdmin, typeController.updateType);
+router.post("/", authJwt, isAdmin, typeValidate, typeController.createType);
+router.put("/", authJwt, isAdmin, typeController.updateType);
 
-router.delete('/:id', authJwt, isAdmin, typeController.deleteType);
+router.delete("/:id", authJwt, isAdmin, typeController.deleteType);
 
 export default router;
