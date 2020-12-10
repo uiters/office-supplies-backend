18b0107dfcc2e99f7dfcbdabbd53970dc1c3d44c
Phan Hoang <thanh-hoang.phan@ant-tech.eu>
2020-12-10 10:08:27 +0700

    update get invoices

--- src/mongoose/invoice-detail.mongoose.ts
+++ src/mongoose/invoice-detail.mongoose.ts
@@ -1,41 +1,51 @@
-import moongose from 'mongoose';
-import { IInvoiceDetail, INVOICE_DETAIL_STATUS } from '../models/invoice-detail.model';
+import moongose from "mongoose";
+import {
+  IInvoiceDetail,
+  INVOICE_DETAIL_STATUS,
+} from "../models/invoice-detail.model";
 
 const invoiceDetail = new moongose.Schema(
-    {
-        invoiceId: {
-            type: moongose.Types.ObjectId,
-            required: true,
-        },
-        sellerId: {
-            type: moongose.Types.ObjectId,
-            required: true,
-            ref: 'user'
-        },
-        productId: {
-            type: moongose.Types.ObjectId,
-            required: true,
-            ref: 'product',
-        },
-        quantity: {
-            type: Number,
-            required: true,
-        },
-        total: {
-            type: Number,
-            required: true,
-        },
-        status: {
-            type: Number,
-            required: true,
-            validate: {
-                validator: (value: INVOICE_DETAIL_STATUS) => {
-                    return Object.values(INVOICE_DETAIL_STATUS).includes(value);
-                },
-            },
-        },
+  {
+    invoiceId: {
+      type: moongose.Types.ObjectId,
+      required: true,
     },
-    { timestamps: true }
+    sellerId: {
+      type: moongose.Types.ObjectId,
+      required: true,
+      ref: "user",
+    },
+    productId: {
+      type: moongose.Types.ObjectId,
+      required: true,
+      ref: "product",
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: Number,
+      required: true,
+      validate: {
+        validator: (value: INVOICE_DETAIL_STATUS) => {
+          return Object.values(INVOICE_DETAIL_STATUS).includes(value);
+        },
+      },
+    },
+  },
+  { timestamps: true }
 );
 
-export const InvoiceDetailModel = moongose.model<IInvoiceDetail>('invoicedetail', invoiceDetail);
+invoiceDetail.pre("find", function () {
+  this.populate("sellerId", "email").populate("productId", "productName");
+});
+
+export const InvoiceDetailModel = moongose.model<IInvoiceDetail>(
+  "invoicedetail",
+  invoiceDetail
+);
